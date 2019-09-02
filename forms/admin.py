6844d428c08d53c67e8bdac87f647c2e79ac7a47
Joshua Paul Chan <joshpaulchan@gmail.com>
2019-09-02 18:06:04 -0400

    Add Form, Submission, SubmissionEntry to admin

--- forms/admin.py
+++ forms/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Form, Submission, SubmissionEntry
 
 # Register your models here.
+
+admin.site.register(Form)
+admin.site.register(Submission)
+admin.site.register(SubmissionEntry)
