caef7f0f5e9f7e324198eb0ec20c7125571bc6a1
Joshua Paul Chan <joshpaulchan@gmail.com>
2019-09-02 19:46:07 -0400

    Nest submissions + fields in form and submission, respectively

--- forms/admin.py
+++ forms/admin.py
@@ -3,6 +3,26 @@ from .models import Form, Submission, SubmissionEntry
 
 # Register your models here.
 
-admin.site.register(Form)
-admin.site.register(Submission)
-admin.site.register(SubmissionEntry)
+
+class SubmissionInline(admin.TabularInline):
+    model = Submission
+    extra = 3
+
+
+class FormAdmin(admin.ModelAdmin):
+    inlines = [SubmissionInline]
+
+
+admin.site.register(Form, FormAdmin)
+
+
+class SubmissionEntryInline(admin.TabularInline):
+    model = SubmissionEntry
+    extra = 0
+
+
+class SubmissionAdmin(admin.ModelAdmin):
+    inlines = [SubmissionEntryInline]
+
+
+admin.site.register(Submission, SubmissionAdmin)
