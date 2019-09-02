bcbc006340d0f1879bf75e0d17878714344b8e7e
Joshua Paul Chan <joshpaulchan@gmail.com>
2019-09-02 14:09:15 -0400

    Add db settings from env

--- formula/settings.py
+++ formula/settings.py
@@ -73,10 +73,16 @@ WSGI_APPLICATION = 'formula.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+import os
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME", ""),
+        'USER': os.getenv("DB_USER", ""),
+        'PASSWORD': os.getenv("DB_PASSWORD", ""),
+        'HOST': os.getenv("DB_HOST", "127.0.0.1"),
+        'PORT': os.getenv("DB_PORT", "5432"), 
     }
 }
 
