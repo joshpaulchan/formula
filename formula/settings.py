7978c6ba32e0bb3a52784df088f8d5e583877d3c
Joshua Paul Chan <joshpaulchan@gmail.com>
2019-09-02 20:22:55 -0400

    Use postgres backend

--- formula/settings.py
+++ formula/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "formula.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME", "formula"),
+        "USER": os.getenv("DB_USER", "formula"),
+        "PASSWORD": os.getenv("DB_PASS"),
+        "HOST": os.getenv("DB_HOST", "127.0.0.1"),
+        "PORT": os.getenv("DB_PORT", "5432"),
     }
 }
 
