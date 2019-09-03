15d34d1749631538765bd5fe62d1cbb5c7fb1045
Joshua Paul Chan <joshpaulchan@gmail.com>
2019-09-02 20:14:09 -0400

    Use above usecase in submit_form

--- forms/views.py
+++ forms/views.py
@@ -1,5 +1,10 @@
+import json
+
 from django.http import HttpResponse
 from django.shortcuts import render
+
+from .models import FormRepository, SubmissionRepository
+from .usecases import SubmitFormUseCase, SubmissionIsNotFalsey
 
 # Create your views here.
 
@@ -8,5 +13,16 @@ def index(request):
     return HttpResponse("Hello")
 
 
+form_repository = FormRepository()
+submission_repository = SubmissionRepository()
+submission_validator = SubmissionIsNotFalsey()
+submit_form_use_case = SubmitFormUseCase(
+    form_repository=form_repository,
+    submission_repository=submission_repository,
+    submission_validator=submission_validator,
+)
+
+
 def submit_form(request, form_id):
-    return HttpResponse(form_id)
+    entries = json.loads(request.body)
+    return HttpResponse(submit_form_use_case(form_id, entries))
